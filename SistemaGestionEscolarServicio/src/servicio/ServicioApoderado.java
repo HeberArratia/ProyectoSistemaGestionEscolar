c79ba512c02e44e32df74544698a6da1732dba01
Heber Arratia <heber.im87@gmail.com>
2015-08-08 20:15:52 -0300

    Jsp completado

--- SistemaGestionEscolarServicio/src/servicio/ServicioApoderado.java
+++
@@ -1,9 +0,0 @@
-package servicio;
-
-import controlador.persona.Apoderado;
-
-public class ServicioApoderado {
-		public String crearApoderado(String nombre,String apellido,String rut,String pass){
-			return Apoderado.agregarNuevoApoderado(nombre, apellido, rut, pass);
-		}
-}
